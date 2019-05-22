c86b22b6b805f8195f0cb05c118c48a74a825383
S-North <stuart.north@gmail.com>
2019-05-22 17:57:53 +0100

    add file GspreadCnnect

---
+++ GoogleSheetsGspreadConnect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('Python test data - cleaning').sheet1
+results = sheet.get_all_records()
+
+pprint(results)
